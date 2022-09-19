e8d25a181af60a0fdf4a5cd56346f2ac2409e365
rezahasetiaputra <reza.putra@vidio.com>
2022-09-19 22:01:34 +0700

    Add more contextual

--- testAPI/test.js
+++ testAPI/test.js
@@ -1,37 +1,44 @@
 const { spec } = require('pactum');
 var expectedSchema = require('./json-schema.json')
 
-it('should get a response with status code 200 and return 20 data', async () => {
-  await spec()
-    .get('https://api.punkapi.com/v2/beers')
-    .withQueryParams('page',2)
-    .withQueryParams('per_page',20)
-    .expectStatus(200)
-    .expectJsonLength(20)
-});
+describe('API Testing',() => {
+  context('Verify User Data', () => {
+    it('should get a response with status code 200 and return 20 data', async () => {
+      await spec()
+        .get('https://api.punkapi.com/v2/beers')
+        .withQueryParams('page',2)
+        .withQueryParams('per_page',20)
+        .expectStatus(200)
+        .expectJsonLength(20)
+    });
 
-it('should get a response with status code 200 and return 5 data', async () => {
-  await spec()
-    .get('https://api.punkapi.com/v2/beers')
-    .withQueryParams('page',2)
-    .withQueryParams('per_page',5)
-    .expectStatus(200)
-    .expectJsonLength(5)
-});
+    it('should get a response with status code 200 and return 5 data', async () => {
+      await spec()
+        .get('https://api.punkapi.com/v2/beers')
+        .withQueryParams('page',2)
+        .withQueryParams('per_page',5)
+        .expectStatus(200)
+        .expectJsonLength(5)
+    });
 
-it('should get a response with status code 200 and return 1 data', async () => {
-  await spec()
-    .get('https://api.punkapi.com/v2/beers')
-    .withQueryParams('page',2)
-    .withQueryParams('per_page',1)
-    .expectStatus(200)
-    .expectJsonLength(1)
-});
+    it('should get a response with status code 200 and return 1 data', async () => {
+      await spec()
+        .get('https://api.punkapi.com/v2/beers')
+        .withQueryParams('page',2)
+        .withQueryParams('per_page',1)
+        .expectStatus(200)
+        .expectJsonLength(1)
+    });
+  });
 
-it('should get a response with status code 200 and Match With Schema Json', async () => {
-  await spec()
-    .get('https://api.punkapi.com/v2/beers')
-    .expectStatus(200)
-    .expectJsonLength(25)
-    .expectJsonSchema(expectedSchema)
+  context('Verify Schema Json',() => {
+    it('should get a response with status code 200 and Match With Schema Json', async () => {
+      await spec()
+        .get('https://api.punkapi.com/v2/beers')
+        .expectStatus(200)
+        .expectJsonLength(25)
+        .expectJsonSchema(expectedSchema)
+    });
+  });
+
 });
